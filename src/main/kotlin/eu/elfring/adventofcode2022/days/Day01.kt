85572beb6c822ea7f83ce24bffa153737b185da4
Tom <1036527+tomelfring@users.noreply.github.com>
2022-12-01 08:55:06 +0100

    Day 1: Calorie Counting (#1)

--- src/main/kotlin/eu/elfring/adventofcode2022/days/Day01.kt
+++ src/main/kotlin/eu/elfring/adventofcode2022/days/Day01.kt
@@ -6,11 +6,17 @@ class Day01 : AoCPuzzle(1)
 {
     override fun part1(input: List<String>): Any
     {
-        return 0
+        val caloriesPerElf = input.joinToString("\n").split("\n\n")
+            .map { elfInventory -> elfInventory.split("\n").sumOf { inventoryItem -> inventoryItem.toLong() } }
+
+        return caloriesPerElf.max()
     }
 
     override fun part2(input: List<String>): Any
     {
-        return 0
+        val caloriesPerElf = input.joinToString("\n").split("\n\n")
+            .map { elfInventory -> elfInventory.split("\n").sumOf { inventoryItem -> inventoryItem.toLong() } }
+
+        return caloriesPerElf.sortedDescending().take(3).sum()
     }
 }
