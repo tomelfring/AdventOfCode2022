628c5c7b76d48ca72435b40544309000160dd6ee
Tom Elfring <tom.elfring@topicus.nl>
2022-12-02 09:16:47 +0100

    Day 2: Rock Paper Scissors

--- src/main/kotlin/eu/elfring/adventofcode2022/days/Day02.kt
+++ src/main/kotlin/eu/elfring/adventofcode2022/days/Day02.kt
@@ -6,11 +6,55 @@ class Day02 : AoCPuzzle(2)
 {
     override fun part1(input: List<String>): Any
     {
-        return 0
+        val scores = mapOf(
+            "%s %s".format(OPPONENT_ROCK,    PLAYED_ROCK)    to SCORE_ROCK    + SCORE_DRAW,
+            "%s %s".format(OPPONENT_ROCK,    PLAYED_PAPER)   to SCORE_PAPER   + SCORE_WIN,
+            "%s %s".format(OPPONENT_ROCK,    PLAYED_SCISSOR) to SCORE_SCISSOR + SCORE_LOSS,
+            "%s %s".format(OPPONENT_PAPER,   PLAYED_ROCK)    to SCORE_ROCK    + SCORE_LOSS,
+            "%s %s".format(OPPONENT_PAPER,   PLAYED_PAPER)   to SCORE_PAPER   + SCORE_DRAW,
+            "%s %s".format(OPPONENT_PAPER,   PLAYED_SCISSOR) to SCORE_SCISSOR + SCORE_WIN,
+            "%s %s".format(OPPONENT_SCISSOR, PLAYED_ROCK)    to SCORE_ROCK    + SCORE_WIN,
+            "%s %s".format(OPPONENT_SCISSOR, PLAYED_PAPER)   to SCORE_PAPER   + SCORE_LOSS,
+            "%s %s".format(OPPONENT_SCISSOR, PLAYED_SCISSOR) to SCORE_SCISSOR + SCORE_DRAW,
+        )
+
+        return input.sumOf { scores[it]!! }
     }
 
     override fun part2(input: List<String>): Any
     {
-        return 0
+        val scores = mapOf(
+            "%s %s".format(OPPONENT_ROCK,    PLAYED_LOSS) to SCORE_SCISSOR + SCORE_LOSS,
+            "%s %s".format(OPPONENT_ROCK,    PLAYED_DRAW) to SCORE_ROCK    + SCORE_DRAW,
+            "%s %s".format(OPPONENT_ROCK,    PLAYED_WIN)  to SCORE_PAPER   + SCORE_WIN,
+            "%s %s".format(OPPONENT_PAPER,   PLAYED_LOSS) to SCORE_ROCK    + SCORE_LOSS,
+            "%s %s".format(OPPONENT_PAPER,   PLAYED_DRAW) to SCORE_PAPER   + SCORE_DRAW,
+            "%s %s".format(OPPONENT_PAPER,   PLAYED_WIN)  to SCORE_SCISSOR + SCORE_WIN,
+            "%s %s".format(OPPONENT_SCISSOR, PLAYED_LOSS) to SCORE_PAPER   + SCORE_LOSS,
+            "%s %s".format(OPPONENT_SCISSOR, PLAYED_DRAW) to SCORE_SCISSOR + SCORE_DRAW,
+            "%s %s".format(OPPONENT_SCISSOR, PLAYED_WIN)  to SCORE_ROCK    + SCORE_WIN,
+        )
+
+        return input.sumOf { scores[it]!! }
+    }
+
+    companion object
+    {
+        const val OPPONENT_ROCK = "A"
+        const val OPPONENT_PAPER = "B"
+        const val OPPONENT_SCISSOR = "C"
+        const val PLAYED_ROCK = "X"
+        const val PLAYED_PAPER = "Y"
+        const val PLAYED_SCISSOR = "Z"
+        const val PLAYED_LOSS = "X"
+        const val PLAYED_DRAW = "Y"
+        const val PLAYED_WIN = "Z"
+
+        const val SCORE_LOSS = 0
+        const val SCORE_DRAW = 3
+        const val SCORE_WIN = 6
+        const val SCORE_ROCK = 1
+        const val SCORE_PAPER = 2
+        const val SCORE_SCISSOR = 3
     }
 }
--- src/test/kotlin/eu/elfring/adventofcode2022/Day02Test.kt
+++ src/test/kotlin/eu/elfring/adventofcode2022/Day02Test.kt
@@ -10,20 +10,22 @@ import org.junit.jupiter.api.Test
 internal class Day02Test
 {
     private val input = """
-        TODO INPUT
-    """.trimIndent().split("\n")
+        A Y
+        B X
+        C Z
+        """.trimIndent().split("\n")
 
     @Test
     @DisplayName("Part 1")
     fun part1()
     {
-        Assertions.assertEquals(0, Day02().part1(input))
+        Assertions.assertEquals(15, Day02().part1(input))
     }
 
     @Test
     @DisplayName("Part 2")
     fun part2()
     {
-        Assertions.assertEquals(0, Day02().part2(input))
+        Assertions.assertEquals(12, Day02().part2(input))
     }
 }
