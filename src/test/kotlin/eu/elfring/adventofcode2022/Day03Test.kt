422b74e0f1f87d0cad90e778e11a9f46c43da272
Tom <1036527+tomelfring@users.noreply.github.com>
2022-12-03 08:29:15 +0100

    Day 3: Rucksack Reorganization (#3)

--- src/test/kotlin/eu/elfring/adventofcode2022/Day03Test.kt
+++ src/test/kotlin/eu/elfring/adventofcode2022/Day03Test.kt
@@ -10,20 +10,25 @@ import org.junit.jupiter.api.Test
 internal class Day03Test
 {
     private val input = """
-        TODO INPUT
+        vJrwpWtwJgWrhcsFMMfFFhFp
+        jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+        PmmdzqPrVvPwwTWBwg
+        wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+        ttgJtRGJQctTZtZT
+        CrZsJsPPZsGzwwsLwLmpwMDw
     """.trimIndent().split("\n")
 
     @Test
     @DisplayName("Part 1")
     fun part1()
     {
-        Assertions.assertEquals(0, Day03().part1(input))
+        Assertions.assertEquals(157, Day03().part1(input))
     }
 
     @Test
     @DisplayName("Part 2")
     fun part2()
     {
-        Assertions.assertEquals(0, Day03().part2(input))
+        Assertions.assertEquals(70, Day03().part2(input))
     }
 }
