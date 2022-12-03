17d3178a8141e630ee1f29a4c85b98380c72a0bc
Tom Elfring <tom.elfring@topicus.nl>
2022-12-03 08:27:14 +0100

    Day 3: Rucksack Reorganization

--- src/main/kotlin/eu/elfring/adventofcode2022/days/Day03.kt
+++ src/main/kotlin/eu/elfring/adventofcode2022/days/Day03.kt
@@ -6,11 +6,43 @@ class Day03 : AoCPuzzle(3)
 {
     override fun part1(input: List<String>): Any
     {
-        return 0
+        return input.sumOf { line ->
+            val (a, b) = line.chunked(line.length/2).map { it.toSet() }
+            val common = a.intersect(b)
+            if (common.size != 1)
+            {
+                throw AssertionError("More than 1 common")
+            }
+
+            val duplicate = common.first()
+            if (duplicate.isLowerCase())
+            {
+                (duplicate.code-96) // a = 97, needs to be 1
+            }
+            else{
+                (duplicate.code-38) // A = 65, needs to be 27
+            }
+        }
     }
 
     override fun part2(input: List<String>): Any
     {
-        return 0
+        return input.chunked(3).sumOf { threeElves ->
+            val (a, b, c) = threeElves.map { it.toSet() }
+            val common = a.intersect(b).intersect(c)
+            if (common.size != 1)
+            {
+                throw AssertionError("More than 1 common")
+            }
+
+            val duplicate = common.first()
+            if (duplicate.isLowerCase())
+            {
+                (duplicate.code-96)
+            }
+            else{
+                (duplicate.code-38)
+            }
+        }
     }
 }
