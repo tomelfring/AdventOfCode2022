61c6bcb1ba4e3a3bc34bfc36f60d31891f6a27f3
Tom <1036527+tomelfring@users.noreply.github.com>
2022-12-04 07:42:29 +0100

    Day 4: Camp Cleanup (#4)

--- src/main/kotlin/eu/elfring/adventofcode2022/days/Day04.kt
+++ src/main/kotlin/eu/elfring/adventofcode2022/days/Day04.kt
@@ -6,11 +6,24 @@ class Day04 : AoCPuzzle(4)
 {
     override fun part1(input: List<String>): Any
     {
-        return 0
+        return input.count {
+            val (a1, a2, b1, b2) = it.split('-', ',').map(String::toInt)
+            val rangeA = a1..a2
+            val rangeB = b1..b2
+
+            val overlapCount = rangeA.intersect(rangeB).count()
+            overlapCount == rangeA.count() || overlapCount == rangeB.count()
+        }
     }
 
     override fun part2(input: List<String>): Any
     {
-        return 0
+        return input.count {
+            val (a1, a2, b1, b2) = it.split('-', ',').map(String::toInt)
+            val rangeA = a1..a2
+            val rangeB = b1..b2
+
+            (rangeA intersect rangeB).isNotEmpty()
+        }
     }
 }
