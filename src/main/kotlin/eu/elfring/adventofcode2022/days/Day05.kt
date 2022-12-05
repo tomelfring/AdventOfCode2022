7797b85b75cd2c0ca45a8a425a84445e8a7c6997
Tom <1036527+tomelfring@users.noreply.github.com>
2022-12-05 10:17:51 +0100

    Day 5: Supply Stacks (#5)

--- src/main/kotlin/eu/elfring/adventofcode2022/days/Day05.kt
+++ src/main/kotlin/eu/elfring/adventofcode2022/days/Day05.kt
@@ -6,11 +6,69 @@ class Day05 : AoCPuzzle(5)
 {
     override fun part1(input: List<String>): Any
     {
-        return 0
+        val cargo = parseCargo(input.takeWhile { it.isNotEmpty() })
+
+        val instructions = input.dropWhile { it.isNotEmpty() }.drop(1)
+
+        instructions.forEach {
+            val (count, from, to) = instructionRegex.matchEntire(it)!!.destructured
+            repeat(count.toInt())
+            {
+                cargo[to.toInt()-1].add(cargo[from.toInt()-1].removeLast())
+            }
+        }
+
+        return cargo.map { it.removeLast() }.joinToString("")
     }
 
     override fun part2(input: List<String>): Any
     {
-        return 0
+        val cargo = parseCargo(input.takeWhile { it.isNotEmpty() })
+
+        val instructions = input.dropWhile { it.isNotEmpty() }.drop(1)
+
+        instructions.forEach {
+            val (count, from, to) = instructionRegex.matchEntire(it)!!.destructured
+            val removed = buildList {
+                repeat(count.toInt())
+                {
+                    add(cargo[from.toInt()-1].removeLast())
+                }
+            }.reversed()
+            cargo[to.toInt()-1].addAll(removed)
+        }
+
+        return cargo.map { it.removeLast() }.joinToString("")
+    }
+
+    private fun parseCargo(input: List<String>): List<ArrayDeque<Char>>
+    {
+        val reversed = input.dropLast(1).reversed()
+        val stackCount = (reversed.first().length+1)/4
+
+        val stacks = buildList<ArrayDeque<Char>> {
+            repeat(stackCount)
+            {
+                add(ArrayDeque())
+            }
+        }
+
+        reversed.forEach {
+            for (i in 0 until ((it.length+1)/4))
+            {
+                val char = it[i*4+1]
+                if (char != ' ')
+                {
+                    stacks[i].add(char)
+                }
+            }
+        }
+
+        return stacks
+    }
+
+    companion object
+    {
+        private val instructionRegex = """move (\d+) from (\d+) to (\d+)""".toRegex()
     }
 }
--- src/test/kotlin/eu/elfring/adventofcode2022/Day05Test.kt
+++ src/test/kotlin/eu/elfring/adventofcode2022/Day05Test.kt
@@ -10,20 +10,28 @@ import org.junit.jupiter.api.Test
 internal class Day05Test
 {
     private val input = """
-        TODO INPUT
+            [D]    
+        [N] [C]    
+        [Z] [M] [P]
+         1   2   3 
+        
+        move 1 from 2 to 1
+        move 3 from 1 to 3
+        move 2 from 2 to 1
+        move 1 from 1 to 2
     """.trimIndent().split("\n")
 
     @Test
     @DisplayName("Part 1")
     fun part1()
     {
-        Assertions.assertEquals(0, Day05().part1(input))
+        Assertions.assertEquals("CMZ", Day05().part1(input))
     }
 
     @Test
     @DisplayName("Part 2")
     fun part2()
     {
-        Assertions.assertEquals(0, Day05().part2(input))
+        Assertions.assertEquals("MCD", Day05().part2(input))
     }
 }
