3a948c64d7a241e76b3ab5022ba7b0ed4f5f3603
Tom <1036527+tomelfring@users.noreply.github.com>
2022-12-06 07:27:07 +0100

    Day 6: Tuning Trouble (#6)

--- src/main/kotlin/eu/elfring/adventofcode2022/days/Day06.kt
+++ src/main/kotlin/eu/elfring/adventofcode2022/days/Day06.kt
@@ -6,11 +6,15 @@ class Day06 : AoCPuzzle(6)
 {
     override fun part1(input: List<String>): Any
     {
-        return 0
+        return findMarker(input.first(), 4)
     }
 
     override fun part2(input: List<String>): Any
     {
-        return 0
+        return findMarker(input.first(), 14)
     }
+
+    private fun findMarker(line: String, distinctCharacters: Int) = line.windowed(distinctCharacters).withIndex()
+        .first { it.value.toSet().size == distinctCharacters }
+        .index.plus(distinctCharacters)
 }
