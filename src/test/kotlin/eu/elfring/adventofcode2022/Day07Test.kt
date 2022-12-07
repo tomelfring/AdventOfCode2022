acc176765a9bb8aecc089f90f0e6a2f0b48fc9e3
Tom <1036527+tomelfring@users.noreply.github.com>
2022-12-07 08:59:22 +0100

    Day 7: No Space Left On Device (#7)

--- src/test/kotlin/eu/elfring/adventofcode2022/Day07Test.kt
+++ src/test/kotlin/eu/elfring/adventofcode2022/Day07Test.kt
@@ -10,20 +10,42 @@ import org.junit.jupiter.api.Test
 internal class Day07Test
 {
     private val input = """
-        TODO INPUT
+        ${'$'} cd /
+        ${'$'} ls
+        dir a
+        14848514 b.txt
+        8504156 c.dat
+        dir d
+        ${'$'} cd a
+        ${'$'} ls
+        dir e
+        29116 f
+        2557 g
+        62596 h.lst
+        ${'$'} cd e
+        ${'$'} ls
+        584 i
+        ${'$'} cd ..
+        ${'$'} cd ..
+        ${'$'} cd d
+        ${'$'} ls
+        4060174 j
+        8033020 d.log
+        5626152 d.ext
+        7214296 k
     """.trimIndent().split("\n")
 
     @Test
     @DisplayName("Part 1")
     fun part1()
     {
-        Assertions.assertEquals(0, Day07().part1(input))
+        Assertions.assertEquals(95437, Day07().part1(input))
     }
 
     @Test
     @DisplayName("Part 2")
     fun part2()
     {
-        Assertions.assertEquals(0, Day07().part2(input))
+        Assertions.assertEquals(24933642, Day07().part2(input))
     }
 }
