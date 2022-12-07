5229152058e503d3061da6a19050ba9e400b6701
Tom Elfring <tom.elfring@topicus.nl>
2022-12-07 08:57:19 +0100

    Day 7: No Space Left On Device

--- src/main/kotlin/eu/elfring/adventofcode2022/days/Day07.kt
+++ src/main/kotlin/eu/elfring/adventofcode2022/days/Day07.kt
@@ -6,11 +6,59 @@ class Day07 : AoCPuzzle(7)
 {
     override fun part1(input: List<String>): Any
     {
-        return 0
+        val directorySizes = getDirectorySizes(input)
+        return directorySizes.values.filter { it < 100_000 }.sum()
     }
 
     override fun part2(input: List<String>): Any
     {
-        return 0
+        val directorySizes = getDirectorySizes(input)
+
+        val spaceToClear = NEEDED_DISK_SPACE - (TOTAL_DISK_SPACE - directorySizes["/"]!!)
+        return directorySizes.values.filter { it > spaceToClear }.min()
+    }
+
+    private fun getDirectorySizes(instructions: List<String>): Map<String, Int>
+    {
+        val directorySizes = mutableMapOf<String, Int>()
+        val stack = mutableListOf<String>()
+
+        instructions.forEach {
+            val splitted = it.split(" ")
+            if (splitted[1] == "cd")
+            {
+                if (splitted[2] == "..")
+                {
+                    stack.removeLast()
+                }
+                else
+                {
+                    val dir = if (stack.isEmpty()) splitted[2] else stack.last()+"/"+splitted[2]
+
+                    if (directorySizes[dir] != null)
+                    {
+                        throw AssertionError("Expected directory names to be unique")
+                    }
+
+                    stack.add(dir)
+                    directorySizes[dir] = 0
+                }
+            }
+            else if (splitted[0].toIntOrNull() != null)
+            {
+                val filesize = splitted[0].toInt()
+                stack.forEach { stackItem ->
+                    directorySizes[stackItem] = directorySizes[stackItem]!!+filesize
+                }
+            }
+        }
+
+        return directorySizes
+    }
+
+    companion object
+    {
+        private const val TOTAL_DISK_SPACE = 70_000_000
+        private const val NEEDED_DISK_SPACE = 30_000_000
     }
 }
