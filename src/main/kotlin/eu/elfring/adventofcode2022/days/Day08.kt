04386d80d05b535a1a37f9fe4b3d53109539530d
Tom <1036527+tomelfring@users.noreply.github.com>
2022-12-08 09:17:08 +0100

    Day 8: Treetop Tree House (#8)

--- src/main/kotlin/eu/elfring/adventofcode2022/days/Day08.kt
+++ src/main/kotlin/eu/elfring/adventofcode2022/days/Day08.kt
@@ -6,11 +6,133 @@ class Day08 : AoCPuzzle(8)
 {
     override fun part1(input: List<String>): Any
     {
-        return 0
+        val trees = input.map { row ->
+            row.toCharArray().map { treeChar -> treeChar.digitToInt() }
+        }
+
+        val visibleTrees = mutableSetOf<Pair<Int, Int>>()
+
+        // Horizontal
+        trees.forEachIndexed { yPos, row ->
+            var highestTree: Int? = null
+
+            // Left to right
+            row.forEachIndexed { xPos, treeInt ->
+                if (highestTree == null || treeInt > highestTree!!)
+                {
+                    highestTree = treeInt
+                    visibleTrees.add(xPos to yPos)
+                }
+            }
+
+            // Right to left
+            highestTree = null
+            row.reversed().forEachIndexed { xPosInvert, treeInt ->
+                if (highestTree == null || treeInt > highestTree!! )
+                {
+                    highestTree = treeInt
+                    visibleTrees.add((row.size-1-xPosInvert) to yPos)
+                }
+            }
+        }
+
+        // Vertical
+        for (x in 0.until(trees.first().size))
+        {
+            var highestTree: Int? = null
+            // Top to bottom
+            for (y in trees.indices)
+            {
+                val treeInt = trees[y][x]
+                if (highestTree == null || treeInt > highestTree!! )
+                {
+                    highestTree = treeInt
+                    visibleTrees.add(x to y)
+                }
+            }
+
+            // Bottom to top
+            highestTree = null
+            for (y in (trees.size-1) downTo 0)
+            {
+                val treeInt = trees[y][x]
+                if (highestTree == null || treeInt > highestTree!! )
+                {
+                    highestTree = treeInt
+                    visibleTrees.add(x to y)
+                }
+            }
+        }
+
+        return visibleTrees.size
     }
 
     override fun part2(input: List<String>): Any
     {
-        return 0
+        val trees = input.map { row ->
+            row.toCharArray().map { treeChar -> treeChar.digitToInt() }
+        }
+
+        var highestScenicScore = 0
+
+        trees.forEachIndexed { yPos, row ->
+            row.forEachIndexed { xPos, treeInt ->
+                var scenicScore = 0
+                var treeCount = 0
+                // Left to right
+                for (lookingXPos in xPos+1 until row.size)
+                {
+                    treeCount++
+                    if (trees[yPos][lookingXPos]>=treeInt)
+                    {
+                        break
+                    }
+                }
+                scenicScore = treeCount
+
+                // Right to left
+                treeCount = 0
+                for (lookingXPos in xPos-1 downTo 0)
+                {
+                    treeCount++
+                    if (trees[yPos][lookingXPos]>=treeInt)
+                    {
+                        break
+                    }
+                }
+                scenicScore *= treeCount
+
+                // Top to bottom
+                treeCount = 0
+                for (lookingYPos in yPos+1 until trees.size)
+                {
+                    treeCount++
+                    if (trees[lookingYPos][xPos]>=treeInt)
+                    {
+                        break
+                    }
+                }
+                scenicScore *= treeCount
+
+                // Bottom to top
+                treeCount = 0
+                for (lookingYPos in yPos-1 downTo 0)
+                {
+                    treeCount++
+                    if (trees[lookingYPos][xPos]>=treeInt)
+                    {
+                        break
+                    }
+                }
+                scenicScore *= treeCount
+
+                if (scenicScore > highestScenicScore)
+                {
+                    highestScenicScore = scenicScore
+                }
+            }
+        }
+
+        return highestScenicScore
     }
 }
--- src/test/kotlin/eu/elfring/adventofcode2022/Day08Test.kt
+++ src/test/kotlin/eu/elfring/adventofcode2022/Day08Test.kt
@@ -10,20 +10,24 @@ import org.junit.jupiter.api.Test
 internal class Day08Test
 {
     private val input = """
-        TODO INPUT
+        30373
+        25512
+        65332
+        33549
+        35390
     """.trimIndent().split("\n")
 
     @Test
     @DisplayName("Part 1")
     fun part1()
     {
-        Assertions.assertEquals(0, Day08().part1(input))
+        Assertions.assertEquals(21, Day08().part1(input))
     }
 
     @Test
     @DisplayName("Part 2")
     fun part2()
     {
-        Assertions.assertEquals(0, Day08().part2(input))
+        Assertions.assertEquals(8, Day08().part2(input))
     }
 }
