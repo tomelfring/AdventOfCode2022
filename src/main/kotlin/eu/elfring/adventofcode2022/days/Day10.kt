2dc8d6f153376ddff46e667f88391a1f8773b620
Tom <1036527+tomelfring@users.noreply.github.com>
2022-12-10 09:19:16 +0100

    Day 10: Cathode-Ray Tube (#10)

--- src/main/kotlin/eu/elfring/adventofcode2022/days/Day10.kt
+++ src/main/kotlin/eu/elfring/adventofcode2022/days/Day10.kt
@@ -6,11 +6,78 @@ class Day10 : AoCPuzzle(10)
 {
     override fun part1(input: List<String>): Any
     {
-        return 0
+        val instructions = ArrayDeque(input)
+        var waiting = false
+        var registerX = 1
+        var signalStrength = 0
+
+        for (cycle in 1..220)
+        {
+            // Read before instruction
+            if ((cycle-20)%40==0)
+            {
+                signalStrength += cycle*registerX
+            }
+
+            // *After* two cycles, the X register is increased by the value V.
+            val instruction = instructions.first()
+
+            if (instruction == "noop" || waiting)
+            {
+                instructions.removeFirst()
+                if (waiting)
+                {
+                    waiting = false
+                    registerX += instruction.split(" ").last().toInt()
+                }
+            }
+            else
+            {
+                waiting = true
+            }
+        }
+
+        return signalStrength
     }
 
     override fun part2(input: List<String>): Any
     {
-        return 0
+        val instructions = ArrayDeque(input)
+        var waiting = false
+        var registerX = 1
+        val crtScreen = mutableListOf<Char>()
+
+        for (cycle in 1..240)
+        {
+            // Draw
+            val drawLocation = (cycle-1)%40 // During cycle 1: CRT draws pixel in position 0
+            if (drawLocation in registerX-1..registerX+1)
+            {
+                crtScreen.add('#')
+            }
+            else
+            {
+                crtScreen.add(' ')
+            }
+
+            // End of cycle, execute instruction
+            val instruction = instructions.first()
+
+            if (instruction == "noop" || waiting)
+            {
+                instructions.removeFirst()
+                if (waiting)
+                {
+                    waiting = false
+                    registerX += instruction.split(" ").last().toInt()
+                }
+            }
+            else
+            {
+                waiting = true
+            }
+        }
+
+        return "\n${crtScreen.joinToString("").chunked(40).joinToString("\n")}"
     }
 }
