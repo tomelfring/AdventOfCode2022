d034213660011b89e807baead622bd63cc4e8cce
Tom <1036527+tomelfring@users.noreply.github.com>
2022-12-11 10:16:45 +0100

    Day 11: Monkey in the Middle (#11)

--- src/main/kotlin/eu/elfring/adventofcode2022/days/Day11.kt
+++ src/main/kotlin/eu/elfring/adventofcode2022/days/Day11.kt
@@ -6,11 +6,82 @@ class Day11 : AoCPuzzle(11)
 {
     override fun part1(input: List<String>): Any
     {
-        return 0
+        val monkeys = input.joinToString("\n").split("\n\n").map(::parseMonkey)
+        val monkeyInspects = playGame(monkeys, 20, true)
+        return monkeyInspects.sortedDescending().take(2).reduce { acc, i -> acc*i }
     }
 
     override fun part2(input: List<String>): Any
     {
-        return 0
+        val monkeys = input.joinToString("\n").split("\n\n").map(::parseMonkey)
+        val monkeyInspects = playGame(monkeys, 10_000, false)
+        return monkeyInspects.sortedDescending().take(2).reduce { acc, i -> acc*i }
+    }
+
+    private fun parseMonkey(monkeyString: String): Monkey
+    {
+        val id = monkeyIdRegex.find(monkeyString)!!.groupValues.last().toInt()
+        val items = monkeyItemsRegex.find(monkeyString)!!.groupValues.last().split(", ").map { it.toLong() }.toMutableList()
+
+        val (operator, numberOrOld) = monkeyOperationRegex.find(monkeyString)!!.groupValues.toList().drop(1)
+        val testValue = monkeyTestRegex.find(monkeyString)!!.groupValues.last().toLong()
+        val ifTrue = monkeyTrueRegex.find(monkeyString)!!.groupValues.last().toInt()
+        val ifFalse = monkeyFalseRegex.find(monkeyString)!!.groupValues.last().toInt()
+
+        val operation = when (operator)
+        {
+            "*" -> { i: Long -> i * (if (numberOrOld == "old") i else numberOrOld.toLong()) }
+            "+" -> { i: Long -> i + (if (numberOrOld == "old") i else numberOrOld.toLong()) }
+            else -> throw NotImplementedError("Operation not implemented")
+        }
+        val test = { i: Long ->  if (i % testValue == 0L) ifTrue else ifFalse }
+
+        return Monkey(id, items, operation, testValue, test)
+    }
+
+    /**
+     * Returns list of inspects per Monkey
+     */
+    private fun playGame(monkeys: List<Monkey>, turns: Int, relieve: Boolean): List<Long>
+    {
+        val monkeyInspects = monkeys.map { 0L }.toMutableList()
+
+        val kindaLcm = monkeys.map { it.testValue }.reduce { acc, l -> acc*l }
+
+        repeat(turns)
+        {
+            monkeys.forEach { monkey ->
+                while (monkey.items.any())
+                {
+                    // Count inspections
+                    monkeyInspects[monkey.id] = monkeyInspects[monkey.id]+1
+
+                    val item = monkey.items.removeFirst()
+                    val worryLevel = if (relieve) monkey.operation(item) / 3 % kindaLcm else monkey.operation(item) % kindaLcm
+                    val monkeyToTrow = monkey.test(worryLevel)
+                    monkeys[monkeyToTrow].items.add(worryLevel)
+                }
+            }
+        }
+
+        return monkeyInspects
+    }
+
+    data class Monkey(
+        val id: Int,
+        val items: MutableList<Long>,
+        val operation: (Long) -> Long,
+        val testValue: Long,
+        val test: (Long) -> Int
+    )
+
+    companion object
+    {
+        private val monkeyIdRegex = """Monkey (\d):""".toRegex()
+        private val monkeyItemsRegex = """Starting items: ([\d, ]*)""".toRegex()
+        private val monkeyOperationRegex = """Operation: new = old ([*+]) (\d+|old)""".toRegex()
+        private val monkeyTestRegex = """Test: divisible by (\d+)""".toRegex()
+        private val monkeyTrueRegex = """If true: throw to monkey (\d+)""".toRegex()
+        private val monkeyFalseRegex = """If false: throw to monkey (\d+)""".toRegex()
     }
 }
--- src/test/kotlin/eu/elfring/adventofcode2022/Day11Test.kt
+++ src/test/kotlin/eu/elfring/adventofcode2022/Day11Test.kt
@@ -10,20 +10,46 @@ import org.junit.jupiter.api.Test
 internal class Day11Test
 {
     private val input = """
-        TODO INPUT
+        Monkey 0:
+          Starting items: 79, 98
+          Operation: new = old * 19
+          Test: divisible by 23
+            If true: throw to monkey 2
+            If false: throw to monkey 3
+        
+        Monkey 1:
+          Starting items: 54, 65, 75, 74
+          Operation: new = old + 6
+          Test: divisible by 19
+            If true: throw to monkey 2
+            If false: throw to monkey 0
+        
+        Monkey 2:
+          Starting items: 79, 60, 97
+          Operation: new = old * old
+          Test: divisible by 13
+            If true: throw to monkey 1
+            If false: throw to monkey 3
+        
+        Monkey 3:
+          Starting items: 74
+          Operation: new = old + 3
+          Test: divisible by 17
+            If true: throw to monkey 0
+            If false: throw to monkey 1
     """.trimIndent().split("\n")
 
     @Test
     @DisplayName("Part 1")
     fun part1()
     {
-        Assertions.assertEquals(0, Day11().part1(input))
+        Assertions.assertEquals(10605L, Day11().part1(input))
     }
 
     @Test
     @DisplayName("Part 2")
     fun part2()
     {
-        Assertions.assertEquals(0, Day11().part2(input))
+        Assertions.assertEquals(2713310158L, Day11().part2(input))
     }
 }
