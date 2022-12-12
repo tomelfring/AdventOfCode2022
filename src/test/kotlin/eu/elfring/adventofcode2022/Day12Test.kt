a81446a12fc034724b138dedf47491b1543ac316
Tom <1036527+tomelfring@users.noreply.github.com>
2022-12-12 09:07:24 +0100

    Day 12: Hill Climbing Algorithm (#12)

--- src/test/kotlin/eu/elfring/adventofcode2022/Day12Test.kt
+++ src/test/kotlin/eu/elfring/adventofcode2022/Day12Test.kt
@@ -10,20 +10,24 @@ import org.junit.jupiter.api.Test
 internal class Day12Test
 {
     private val input = """
-        TODO INPUT
+        Sabqponm
+        abcryxxl
+        accszExk
+        acctuvwj
+        abdefghi
     """.trimIndent().split("\n")
 
     @Test
     @DisplayName("Part 1")
     fun part1()
     {
-        Assertions.assertEquals(0, Day12().part1(input))
+        Assertions.assertEquals(31, Day12().part1(input))
     }
 
     @Test
     @DisplayName("Part 2")
     fun part2()
     {
-        Assertions.assertEquals(0, Day12().part2(input))
+        Assertions.assertEquals(29, Day12().part2(input))
     }
 }
