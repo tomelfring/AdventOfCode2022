57cafb0018192a59b7bf5a3c95520d6cfc493576
Tom Elfring <tom.elfring@topicus.nl>
2022-12-13 10:02:48 +0100

    Day 13: Distress Signal

--- src/test/kotlin/eu/elfring/adventofcode2022/Day13Test.kt
+++ src/test/kotlin/eu/elfring/adventofcode2022/Day13Test.kt
@@ -10,20 +10,42 @@ import org.junit.jupiter.api.Test
 internal class Day13Test
 {
     private val input = """
-        TODO INPUT
+        [1,1,3,1,1]
+        [1,1,5,1,1]
+        
+        [[1],[2,3,4]]
+        [[1],4]
+        
+        [9]
+        [[8,7,6]]
+        
+        [[4,4],4,4]
+        [[4,4],4,4,4]
+        
+        [7,7,7,7]
+        [7,7,7]
+        
+        []
+        [3]
+        
+        [[[]]]
+        [[]]
+        
+        [1,[2,[3,[4,[5,6,7]]]],8,9]
+        [1,[2,[3,[4,[5,6,0]]]],8,9]
     """.trimIndent().split("\n")
 
     @Test
     @DisplayName("Part 1")
     fun part1()
     {
-        Assertions.assertEquals(0, Day13().part1(input))
+        Assertions.assertEquals(13, Day13().part1(input))
     }
 
     @Test
     @DisplayName("Part 2")
     fun part2()
     {
-        Assertions.assertEquals(0, Day13().part2(input))
+        Assertions.assertEquals(140, Day13().part2(input))
     }
 }
