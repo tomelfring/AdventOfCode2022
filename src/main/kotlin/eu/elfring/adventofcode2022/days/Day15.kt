aaf9b5e7cb9df10cc519cc9fefb76b2928df3d23
Tom <1036527+tomelfring@users.noreply.github.com>
2022-12-15 20:14:43 +0100

    Day 15: Beacon Exclusion Zone (#15)

--- src/main/kotlin/eu/elfring/adventofcode2022/days/Day15.kt
+++ src/main/kotlin/eu/elfring/adventofcode2022/days/Day15.kt
@@ -1,16 +1,112 @@
 package eu.elfring.adventofcode2022.days
 
 import eu.elfring.adventofcode2022.AoCPuzzle
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
 
 class Day15 : AoCPuzzle(15)
 {
     override fun part1(input: List<String>): Any
     {
-        return 0
+        val toCheck = if (input.size == 14) 10 else 2_000_000
+        val sensors = parseInput(input)
+        val covered = sensors.mapNotNull { it.getCoveredColumnsForRow(toCheck) }.flatten().toSet()
+
+        val beaconsInToCheck = sensors.filter { it.beaconY == toCheck }.map { it.beaconY }
+
+        return covered.filter { it !in beaconsInToCheck }.size
     }
 
     override fun part2(input: List<String>): Any
     {
-        return 0
+        val toCheck = if (input.size == 14) 0..20 else 0..4_000_000
+        val sensors = parseInput(input)
+
+        for (i in toCheck)
+        {
+            val coveredRanges = sensors.mapNotNull { it.getCoveredColumnsForRow(i) }
+            val mergedRanges = mergeRanges(coveredRanges)
+
+            if (mergedRanges.count() > 1)
+            {
+                return (mergedRanges.first().last+1)*4_000_000L + i
+            }
+        }
+
+        return -1
+    }
+
+    private fun parseInput(input: List<String>): List<Sensor>
+    {
+        return input.map {
+            val (x, y, beaconX, beaconY) = inputRegex.matchEntire(it)!!.groupValues.drop(1).map {
+                it.toInt()
+            }
+            Sensor(x, y, beaconX, beaconY)
+        }
+    }
+
+    private fun mergeRanges(ranges: List<IntRange>): List<IntRange>
+    {
+        val sortedRanges = ranges.sortedBy { it.first }
+
+        var start = sortedRanges.first().first
+        var end = sortedRanges.first().last
+        val mergedRanges = mutableListOf<IntRange>()
+        sortedRanges.forEach {
+            // It is in range (or direct next to) && extends our range
+            if (it.first <= end+1 && it.last > end)
+            {
+                end = it.last
+            }
+            // It is a new range
+            else if (it.first > end)
+            {
+                mergedRanges.add(start..end)
+                start = it.first
+                end = it.last
+            }
+        }
+        mergedRanges.add(start..end)
+
+        return mergedRanges
+    }
+
+    data class Sensor(
+        val x: Int,
+        val y: Int,
+        val beaconX: Int,
+        val beaconY: Int
+    )
+    {
+        private val manhattenDistance by lazy {
+            abs(x-beaconX)+abs(y - beaconY)
+        }
+
+        fun getCoveredColumnsForRow(targetY: Int): IntRange?
+        {
+            val rangeTaken = if (targetY >= y)
+            {
+                targetY-y
+            }
+            else
+            {
+                y-targetY
+            }
+
+            val rangeRemaining = manhattenDistance-rangeTaken
+            if (rangeRemaining < 0)
+            {
+                return null
+            }
+
+            return (x-rangeRemaining)..(x+rangeRemaining)
+        }
+    }
+
+    companion object
+    {
+        val inputRegex = """Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)""".toRegex()
     }
 }
--- src/test/kotlin/eu/elfring/adventofcode2022/Day15Test.kt
+++ src/test/kotlin/eu/elfring/adventofcode2022/Day15Test.kt
@@ -10,20 +10,33 @@ import org.junit.jupiter.api.Test
 internal class Day15Test
 {
     private val input = """
-        TODO INPUT
+        Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+        Sensor at x=9, y=16: closest beacon is at x=10, y=16
+        Sensor at x=13, y=2: closest beacon is at x=15, y=3
+        Sensor at x=12, y=14: closest beacon is at x=10, y=16
+        Sensor at x=10, y=20: closest beacon is at x=10, y=16
+        Sensor at x=14, y=17: closest beacon is at x=10, y=16
+        Sensor at x=8, y=7: closest beacon is at x=2, y=10
+        Sensor at x=2, y=0: closest beacon is at x=2, y=10
+        Sensor at x=0, y=11: closest beacon is at x=2, y=10
+        Sensor at x=20, y=14: closest beacon is at x=25, y=17
+        Sensor at x=17, y=20: closest beacon is at x=21, y=22
+        Sensor at x=16, y=7: closest beacon is at x=15, y=3
+        Sensor at x=14, y=3: closest beacon is at x=15, y=3
+        Sensor at x=20, y=1: closest beacon is at x=15, y=3
     """.trimIndent().split("\n")
 
     @Test
     @DisplayName("Part 1")
     fun part1()
     {
-        Assertions.assertEquals(0, Day15().part1(input))
+        Assertions.assertEquals(26, Day15().part1(input))
     }
 
     @Test
     @DisplayName("Part 2")
     fun part2()
     {
-        Assertions.assertEquals(0, Day15().part2(input))
+        Assertions.assertEquals(56000011L, Day15().part2(input))
     }
 }
