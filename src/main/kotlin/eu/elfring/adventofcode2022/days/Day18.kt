0c5b58f146411396f1417aa99e96bc7093a64c66
Tom <1036527+tomelfring@users.noreply.github.com>
2022-12-18 09:31:41 +0100

    Day 18: Boiling Boulders (#18)

--- src/main/kotlin/eu/elfring/adventofcode2022/days/Day18.kt
+++ src/main/kotlin/eu/elfring/adventofcode2022/days/Day18.kt
@@ -6,11 +6,72 @@ class Day18 : AoCPuzzle(18)
 {
     override fun part1(input: List<String>): Any
     {
-        return 0
+        val coordinates = input.map(::parseLine)
+
+        return coordinates.sumOf { it.getDirectNeighbours().count { it !in coordinates } }
     }
 
     override fun part2(input: List<String>): Any
     {
-        return 0
+        val coordinates = input.map(::parseLine)
+
+        val minX = coordinates.minOf { it.x }-1
+        val maxX = coordinates.maxOf { it.x }+1
+        val rangeX = minX..maxX
+
+        val minY = coordinates.minOf { it.y }-1
+        val maxY = coordinates.maxOf { it.y }+1
+        val rangeY = minY..maxY
+
+        val minZ = coordinates.minOf { it.z }-1
+        val maxZ = coordinates.maxOf { it.z }+1
+        val rangeZ = minZ..maxZ
+
+        // BFS
+        val start = Coordinate(minX, minY, minZ)
+        val toVisit = ArrayDeque<Coordinate>()
+        val explored = mutableListOf<Coordinate>()
+        toVisit.add(start)
+
+        var outsideVisibleSides = 0
+
+        while (toVisit.isNotEmpty())
+        {
+            val toCheck = toVisit.removeFirst()
+
+            val neigbours = toCheck.getDirectNeighbours()
+                .filter { it.x in rangeX && it.y in rangeY && it.z in rangeZ }
+                .filter { it !in explored }
+
+            outsideVisibleSides += neigbours.count { it in coordinates }
+
+            neigbours.filter { it !in coordinates && it !in toVisit }.forEach {
+                toVisit.add(it)
+            }
+            explored.add(toCheck)
+        }
+
+        return outsideVisibleSides
+    }
+
+    private fun parseLine(input: String): Coordinate
+    {
+        val (x, y, z) = input.split(",").map { it.toInt() }
+        return Coordinate(x, y, z)
+    }
+
+    data class Coordinate(val x: Int, val y: Int, val z: Int)
+    {
+        fun getDirectNeighbours(): List<Coordinate>
+        {
+            return listOf(
+                Coordinate(x-1, y, z),
+                Coordinate(x+1, y, z),
+                Coordinate(x, y-1, z),
+                Coordinate(x, y+1, z),
+                Coordinate(x, y, z-1),
+                Coordinate(x, y, z+1),
+            )
+        }
     }
 }
--- src/test/kotlin/eu/elfring/adventofcode2022/Day18Test.kt
+++ src/test/kotlin/eu/elfring/adventofcode2022/Day18Test.kt
@@ -10,20 +10,32 @@ import org.junit.jupiter.api.Test
 internal class Day18Test
 {
     private val input = """
-        TODO INPUT
+        2,2,2
+        1,2,2
+        3,2,2
+        2,1,2
+        2,3,2
+        2,2,1
+        2,2,3
+        2,2,4
+        2,2,6
+        1,2,5
+        3,2,5
+        2,1,5
+        2,3,5
     """.trimIndent().split("\n")
 
     @Test
     @DisplayName("Part 1")
     fun part1()
     {
-        Assertions.assertEquals(0, Day18().part1(input))
+        Assertions.assertEquals(64, Day18().part1(input))
     }
 
     @Test
     @DisplayName("Part 2")
     fun part2()
     {
-        Assertions.assertEquals(0, Day18().part2(input))
+        Assertions.assertEquals(58, Day18().part2(input))
     }
 }
