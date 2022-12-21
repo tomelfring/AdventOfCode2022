8df49673380527806ccc97b0c4c3e50fd20bbfa3
Tom Elfring <tom.elfring@topicus.nl>
2022-12-21 16:07:48 +0100

    Day 21: Monkey Math

--- src/test/kotlin/eu/elfring/adventofcode2022/Day21Test.kt
+++ src/test/kotlin/eu/elfring/adventofcode2022/Day21Test.kt
@@ -10,20 +10,34 @@ import org.junit.jupiter.api.Test
 internal class Day21Test
 {
     private val input = """
-        TODO INPUT
+        root: pppw + sjmn
+        dbpl: 5
+        cczh: sllz + lgvd
+        zczc: 2
+        ptdq: humn - dvpt
+        dvpt: 3
+        lfqf: 4
+        humn: 5
+        ljgn: 2
+        sjmn: drzm * dbpl
+        sllz: 4
+        pppw: cczh / lfqf
+        lgvd: ljgn * ptdq
+        drzm: hmdt - zczc
+        hmdt: 32
     """.trimIndent().split("\n")
 
     @Test
     @DisplayName("Part 1")
     fun part1()
     {
-        Assertions.assertEquals(0, Day21().part1(input))
+        Assertions.assertEquals(152L, Day21().part1(input))
     }
 
     @Test
     @DisplayName("Part 2")
     fun part2()
     {
-        Assertions.assertEquals(0, Day21().part2(input))
+        Assertions.assertEquals(301L, Day21().part2(input))
     }
 }
