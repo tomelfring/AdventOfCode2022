986ef099b5d787492ce107c542b2ef50c31ee01e
Tom <1036527+tomelfring@users.noreply.github.com>
2022-12-23 10:53:13 +0100

    Day 22: Monkey Map (#22)

--- src/test/kotlin/eu/elfring/adventofcode2022/Day22Test.kt
+++ src/test/kotlin/eu/elfring/adventofcode2022/Day22Test.kt
@@ -3,6 +3,7 @@ package eu.elfring.adventofcode2022
 import eu.elfring.adventofcode2022.days.Day22
 
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
@@ -10,20 +11,34 @@ import org.junit.jupiter.api.Test
 internal class Day22Test
 {
     private val input = """
-        TODO INPUT
+                ...#
+                .#..
+                #...
+                ....
+        ...#.......#
+        ........#...
+        ..#....#....
+        ..........#.
+                ...#....
+                .....#..
+                .#......
+                ......#.
+        
+        10R5L5R10L4R5L5
     """.trimIndent().split("\n")
 
     @Test
     @DisplayName("Part 1")
     fun part1()
     {
-        Assertions.assertEquals(0, Day22().part1(input))
+        Assertions.assertEquals(6032, Day22().part1(input))
     }
 
+    @Disabled("Code only works on real input, not test inut")
     @Test
     @DisplayName("Part 2")
     fun part2()
     {
-        Assertions.assertEquals(0, Day22().part2(input))
+        Assertions.assertEquals(5031, Day22().part2(input))
     }
 }
