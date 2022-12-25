e52aac317adfe37dfc2bb417ba33fb11cd5fa1bc
Tom Elfring <tom.elfring@topicus.nl>
2022-12-25 08:12:00 +0100

    Day 25: Full of Hot Air

--- src/test/kotlin/eu/elfring/adventofcode2022/Day25Test.kt
+++ src/test/kotlin/eu/elfring/adventofcode2022/Day25Test.kt
@@ -10,20 +10,32 @@ import org.junit.jupiter.api.Test
 internal class Day25Test
 {
     private val input = """
-        TODO INPUT
+        1=-0-2
+        12111
+        2=0=
+        21
+        2=01
+        111
+        20012
+        112
+        1=-1=
+        1-12
+        12
+        1=
+        122
     """.trimIndent().split("\n")
 
     @Test
     @DisplayName("Part 1")
     fun part1()
     {
-        Assertions.assertEquals(0, Day25().part1(input))
+        Assertions.assertEquals("2=-1=0", Day25().part1(input))
     }
 
     @Test
     @DisplayName("Part 2")
     fun part2()
     {
-        Assertions.assertEquals(0, Day25().part2(input))
+        Assertions.assertEquals("Merry Christmas!", Day25().part2(input))
     }
 }
